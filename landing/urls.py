9cbe3b9a894766f786b263abe067704f27d3f41e
Alexei <nod19038@gmail.com>
2019-08-05 19:15:18 +0300

    not working load rules btn

--- landing/urls.py
+++ landing/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from landing import views
+from django.urls import path
 
 
 urlpatterns = [
 
     url(r'landing/', views.landing, name='landing'),
-    url(r'', views.index, name='index')
-
+    url(r'landing/', views.action_url),
+    url(r'', views.index, name='index'),
 
 ]
 
--- landing/views.py
+++ landing/views.py
@@ -2,22 +2,12 @@ from django.shortcuts import render
 from .models import Rule
 from .forms import SubscribersForm
 from datetime import date
+from django.shortcuts import redirect
 from xml.etree import ElementTree as ET
 root = ET.parse('templates/testdata/direct.xml').getroot()
 
 
 def landing(request):
-    ar = []
-    ar.clear()
-    for type_tag in root.iter('rule'):
-        priority = type_tag.get('priority')
-        table = type_tag.get('table')
-        ipv = type_tag.get('ipv')
-        chain = type_tag.get('chain')
-        ar.append(type_tag.text)
-        print(type_tag.text)
-        p = Rule(priority=priority, table=table, ipv=ipv, chain=chain, rule_value=type_tag.text)
-        p.save()
 
     form = SubscribersForm(request.POST or None)
 
@@ -27,8 +17,6 @@ def landing(request):
         save_form = form.save()
 
     today = date.today()
-    allrules = Rule.objects.all()
-    context = {'allrules': allrules}
 
 
     return render(request, 'landing/landing.html', locals())
@@ -37,3 +25,26 @@ def landing(request):
 def index(request):
 
     return render(request, 'index.html', locals())
+
+
+def action_url(request):
+    if request.method('action_url'):
+        ar = []
+        ar.clear()
+        for type_tag in root.iter('rule'):
+            priority = type_tag.get('priority')
+            table = type_tag.get('table')
+            ipv = type_tag.get('ipv')
+            chain = type_tag.get('chain')
+            ar.append(type_tag.text)
+            print(type_tag.text)
+            p = Rule(priority=priority, table=table, ipv=ipv, chain=chain, rule_value=type_tag.text)
+            p.save()
+            return redirect('landing/landing.html')
+
+        allrules = Rule.objects.all()
+        context = {'allrules': allrules}
+
+    return render(request, 'landing/landing.html', locals())
+
+

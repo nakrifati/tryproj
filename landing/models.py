7df142941bc35f932b78d53a82455cf57e116b28
Alexei <nod19038@gmail.com>
2019-08-06 19:03:17 +0300

    Small model changes

--- landing/models.py
+++ landing/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Subscriber(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=128)
+    email = models.EmailField(blank=True)
+    name = models.CharField(max_length=128, blank=True)
 
     def __str__(self):
         return "%s %s %s" % (self.name, self.email, self.id)
@@ -14,7 +14,7 @@ class Rule(models.Model):
     table = models.CharField(max_length=256, default='')
     ipv = models.CharField(max_length=64, default='')
     chain = models.CharField(max_length=256, default='')
-    rule_value = models.CharField(max_length=256, default='')
+    rule_value = models.TextFieldField(max_length=256, default='')
 
     def __str__(self):
         return "%s %s %s %s %s" % (self.priority, self.table, self.ipv, self.chain, self.rule_value)

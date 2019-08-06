46b2f269490dab65b9984452581922342589f522
Unknown <unknown@example.com>
2019-08-06 23:01:23 +0300

    fixed value base bug

--- landing/models.py
+++ landing/models.py
@@ -14,7 +14,7 @@ class Rule(models.Model):
     table = models.CharField(max_length=256, default='')
     ipv = models.CharField(max_length=64, default='')
     chain = models.CharField(max_length=256, default='')
-    rule_value = models.TextFieldField(max_length=256, default='')
+    rule_value = models.CharField(max_length=256, default='')
 
     def __str__(self):
         return "%s %s %s %s %s" % (self.priority, self.table, self.ipv, self.chain, self.rule_value)

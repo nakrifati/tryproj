1d7c5651a3abe96df94e5b1389b0d4b4277a939c
Alexei <nod19038@gmail.com>
2020-02-25 17:09:08 +0300

    Added Revoke Certificate menu

--- Ovpn_users/urls.py
+++ Ovpn_users/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'ovpn_users/', views.ovpn_users, name='ovpn_users'),
     url(r'create_ovpn_user', views.create_ovpn_user, name='create_ovpn_user'),
     url(r'list_ovpn_user', views.list_ovpn_user, name='list_ovpn_user'),
+    url(r'revoke_ovpn_user', views.revoke_ovpn_user, name='revoke_ovpn_user'),
 
 ]
 

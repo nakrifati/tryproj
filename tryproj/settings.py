f3f0b3c217a92d7977c3f291102be0e614a97fc4
Alexei <nod19038@gmail.com>
2020-02-27 14:33:59 +0300

    Added breadcrumbs

--- tryproj/settings.py
+++ tryproj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'landing',
     'ovpn_users',
+    'django_bootstrap_breadcrumbs',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
